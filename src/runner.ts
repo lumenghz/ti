07c0740808a0c274c448b2430474f281741c2241
Dennis Lu <jiahehz@gmail.com>
2022-06-07 19:53:13 +0800

    feat: make task search easily using fzf

---
+++ src/runner.ts
@@ -0,0 +1,43 @@
+import { type SyncOptions, execaCommandSync } from 'execa'
+import prompts, { type Answers } from 'prompts'
+import { Fzf } from 'fzf'
+import { parseCommand } from './parse'
+import type { TiConfig, TiTask } from './config'
+
+function getTask(config: TiConfig, name: string): TiTask {
+  for (const task of config.tasks) {
+    if (task.name === name)
+      return task
+  }
+  // it should never reach here
+  process.exit(1)
+}
+
+export function runTi(config: TiConfig): void {
+  const choices = config.tasks.map(task => ({ title: task.name, value: task }))
+  const fzf = new Fzf(choices.map(choice => choice.title))
+
+  prompts({
+    type: 'autocomplete',
+    name: 'task',
+    message: 'Select a task',
+    choices,
+    suggest: (input: string) => Promise.resolve(fzf.find(input).map(item => item.item)),
+  }).then(async (answer: Answers<'task'>) => {
+    const task = getTask(config, answer.task)
+
+    try {
+      for (const cmd of task.cmds) {
+        let options: SyncOptions = { encoding: 'utf8', stdio: 'inherit', cwd: task.cwd }
+        if (cmd.type === 'shell')
+          options = { ...options, shell: true }
+
+        const command = await parseCommand(cmd)
+        execaCommandSync(command, options)
+      }
+    }
+    catch (e) {
+      console.error(e)
+    }
+  })
+}
