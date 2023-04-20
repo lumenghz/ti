706c70c471ad0833a0ccc9c77c21e6019d35f7aa
Dennis Lu <jiahehz@gmail.com>
2023-04-20 13:55:04 +0800

    chore: update eslint rules to latest

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-import { readFileSync } from 'fs'
-import { join } from 'path'
+import { readFileSync } from 'node:fs'
+import { join } from 'node:path'
 import { parse as parseYaml } from 'yaml'
 import { globbySync } from 'globby'
 
